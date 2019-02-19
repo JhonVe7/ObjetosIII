4c295ab1503300548d1e73ff4df09b3ba8f9b0af
JhonVe7 <32178848+JhonVe7@users.noreply.github.com>
2019-02-19 15:12:09 -0500

    Create x.java

---
+++ x.java
@@ -0,0 +1,32 @@
+				
+public class x {
+	String NombreEmpleado ;
+	x(){}
+	public void setNombreEmpleado(String NombreEmpleado) {
+		this.NombreEmpleado= NombreEmpleado;	
+	}
+	public String getNombreEmpleado() {
+		return NombreEmpleado;
+	}
+	
+	public static void main(String[] args) {
+		String Nombreempleado,nEmpresa, nBanco,Opc;
+		float mPrestamo,iMensual, tPagarPrestamo;
+		String seguir="s";
+		x empleado1 = new x();
+		while(seguir.equals("s")) {
+			 
+			/*menu
+			 * 1. ingreso de dato
+			 * 2. visualizar interés mensual
+			 * 3.visualizar el total a pagar
+			 * 4. terminar
+			 * 
+			 * opc = JOptionPanel.showInput("Difgite su opcion");
+			 * switch(opc){
+			 * ..}
+			 */
+			
+		}
+	}
+}
